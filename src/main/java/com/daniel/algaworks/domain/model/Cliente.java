dd87dd1d8c057ed3ac986fcfa5b3cfa341ffa443
Daniel <danielmrg@gmail.com>
2022-09-16 16:22:07 -0300

    Bean Validation and Exception Handler

--- src/main/java/com/daniel/algaworks/domain/model/Cliente.java
+++ src/main/java/com/daniel/algaworks/domain/model/Cliente.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,8 +21,17 @@ public class Cliente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotBlank
+	@Size(max = 60)
 	private String nome;
+	
+	@NotBlank
+	@Email
 	private String email;
+	
+	@NotBlank
+	@Size(max=20)
 	private String telefone;
 
 }
